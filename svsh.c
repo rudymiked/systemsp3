96076dfd261bd39e62a8bb9701f3234091039ec2
Rudy <mru222@g.uky.edu>
2014-11-13 20:50:29 -0500

    added args  - rudy

--- svsh.c
+++ svsh.c
@@ -2,38 +2,71 @@
 // Mike Rudy
 // CS 485
 
+#include "svsh.h"
+//#include "y.tab.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-
-#include "scanner.h"
-
-#define MAX_JOBS 20
 
 char *prompt;
 
-int call() {
+//int call(int opr, int nops, ..) {
+int call(int opr, ...) {
   char str[60];
   int i = 0;
   char *jobs[MAX_JOBS];
+  char wd[100];
   prompt = "svsh";
 
   printf("CS Shared Variable Shell\n");
   printf("%s > ", prompt);
   //scanf("%s", str);
   //printf("newstr: %s\n", str);
-  fgets(str, sizeof(str), stdin);
-  str[strlen(str) - 1 ] = '\0';
+  //fgets(str, sizeof(str), stdin);
+  //str[strlen(str) - 1 ] = '\0';
 
+  while (opr != BYE) {
+    switch(opr) {
+      case (METACHAR)
+        printf("%s\n", str);
+      case (DEFPROMPT) {
+        prompt = "new_svsh";
+      }
+      case (LISTJOBS) {
+        int j, n;
+        n = sizeof(jobs)/sizeof(jobs[0]);
+        for(j = 0; j < n; j++){
+          printf("%s %d\n", jobs[i], i);
+        }
+      }
+      case (CD) {
+        getwd(wd);
+        printf("wd: %s \n", wd);
+        chdir("/home/");
+        getwd(wd);
+        printf("wd: %s \n", wd);
+      }
+      case (RUN) {
+        printf("Run program \n");
+      }
+      case (ASSIGNTO) {
+        printf("assign output to a variable\n");
+      }
+      default
+        printf("'%s' : Not A Command\n", str);
+    printf("%s > ", prompt);
+    fgets(str, sizeof(str), stdin);
+    
+    str[strlen(str) - 1 ] = '\0';
+  }
+/*
   while (strcmp(str, "bye") != 0) {
     if (strcmp(str, "pwd") == 0)
-      printf("Present Working Directory\n");
-    else if (str[0] == '#')
+      printf("Present Working Directory\n"); 
+    else if (str[0] == '#') {
       printf("%s\n", str);
-    else if (strcmp(str, "defprompt") == 0)
+    }
+    else if (strcmp(str, "defprompt") == 0) {
       prompt = "new_svsh";
+    }
     else if (strcmp(str, "listjobs") == 0) {
       int j, n;
       n = sizeof(jobs)/sizeof(jobs[0]);
@@ -41,13 +74,19 @@ int call() {
         printf("%s %d\n", jobs[i], i);
       }
     }
-    else if (strcmp(str, "cd") == 0) 
-      printf("Changing directories\n");
+    else if (strcmp(str, "cd") == 0) { 
+      getwd(wd);
+      printf("wd: %s \n", wd);
+      chdir("/home/");
+      getwd(wd);
+      printf("wd: %s \n", wd);
+    }
     else if (strcmp(str, "run") == 0) {
       printf("Run program \n");
     }
-    else if (strcmp(str, "assignto") == 0)
+    else if (strcmp(str, "assignto") == 0) {
       printf("assign output to a variable\n");
+    }
     else
         printf("'%s' : Not A Command\n", str);
     printf("%s > ", prompt);
@@ -56,14 +95,15 @@ int call() {
     str[strlen(str) - 1 ] = '\0';
     
   }
-
+*/
   return 0;
 }
-
+/*
 int main() {
-  
+   
   call();  
 
 return 0;
 
 }
+*/
